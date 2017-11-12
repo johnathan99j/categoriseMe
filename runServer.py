4634e47e6d26fb0a17c56a75657cf6f0c76986cb
Johnathan Graydon <johnathan99j@yahoo.com>
2017-11-12 11:48:27 +0000

    Add interaction from view to controller

--- runServer.py
+++ runServer.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -6,15 +7,23 @@ app = Flask(__name__)
 def index():
     a_sentence = Sentence("hi there")
     message = {}
-    message['text'] = a_sentence.text
     return render_template('index.html', message=message)
+
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    # os.system("./scripts/sentimentAnalyser.py " + str(request.data))
+    message = {}
+    message['text'] = str(request.POST.get("inputText"))
+    return render_template('index.html', message=message)
+
 
 class Sentence():
     def __init__(self, text):
         self.text = text
-        self.sentiment = 100 # -1 for negative, 0 for neutral, 1 for positive,
-                            # 100 for default
-        self.category = '' # empty string by default
+        self.sentiment = 100  # -1 for negative, 0 for neutral, 1 for positive, 100 for default
+
+        self.category = ''  # empty string by default
 
     def set_sentiment(self, sentiment):
         this.sentiment = sentiment
@@ -24,5 +33,7 @@ class Sentence():
 
     def __repr__(self):
         return '<Sentence: %r >' % (self.text)
+
+
 if __name__ == '__main__':
     app.run()
